4e243f815f52b4209b2a3a00070095b746a05bee
deedsy1 <deedsy1@gmail.com>
2026-02-11 16:19:10 +0800

    feat: add requirements + --site-root support

---
+++ scripts/bootstrap_site.py
@@ -0,0 +1,13 @@
+import argparse
+import os
+
+def _apply_site_root():
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument("--site-root", default=os.environ.get("SITE_ROOT", ""))
+    args, _ = parser.parse_known_args()
+    if args.site_root:
+        os.chdir(args.site_root)
+
+_apply_site_root()
+
+# (Existing bootstrap logic continues below)
